bd8a246d7748cb499611b6c2fb7854bdfe193f5d
smallonestep <hankwan777@gmail.com>
2018-06-17 13:48:19 +0900

    while{while}을 종료시키기 위해 메소드로 만들어 return 활용

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -15,6 +15,41 @@ public class Calendar {
 		}
 	}
 	
+	public void cal() {
+		String prompt = "cal>";
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("연도를 입력하세요");
+			System.out.println(prompt);
+			int inputYear = sc.nextInt();
+			if(inputYear == -1) {
+				sc.close();
+				System.out.println("종료합니다");
+				return;
+			}else if(inputYear < 0) {
+				System.out.println("양수를 입력하세요");
+				continue;
+			}else {
+				while(true) {
+					System.out.println("달을 입력하세요.(1~12)");
+					System.out.println(prompt);
+					int inputMonth = sc.nextInt();
+					if(inputMonth == -1) {
+						sc.close();
+						System.out.println("종료합니다");
+						return;
+					}
+					if(inputMonth > 12 || inputMonth <= 0) {
+						System.out.println("1~12사이의 수를 입력하세요");
+						continue;
+					}
+					System.out.printf("%d년 %d월은 %d일 까지 있습니다. \n\n", inputYear, inputMonth, maxDayOfMonth(inputYear, inputMonth));
+					break;
+				}
+			}
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		// System.out.println("Hello, Calendar!");
@@ -40,21 +75,44 @@ public class Calendar {
 		//최대일수는 정해진 상수이므로 static으로 빼서 사용해보기
 		//메소드 만들어 사용해보기
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("반복할 횟수를 입력하세요");
-		int repeat = sc.nextInt();
+		//String prompt = "cal>";
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("반복할 횟수를 입력하세요");
+		//System.out.println(prompt);
+		//int repeat = sc.nextInt();
+		
 		Calendar cal = new Calendar();
 		
+		//연도는 제대로 입력했는데 달 입력범위를 벗어났을 경우 while을 종료시키기 위해 메소드로 만들고 조건분기에서 return을 활용했다.
+		cal.cal();
+		
+		
 		//n번 반복하도록 바꾸기
-		for (int i = 0; i < repeat; i++) {
+		//for (int i = 0; i < repeat; i++) {
+		/*int inputYear = 0;
+		while(true) {
 			System.out.println("연도를 입력하세요");
-			int inputYear = sc.nextInt();
-			System.out.println("달을 입력하세요.(1~12)");
-			int inputMonth = sc.nextInt();
-			System.out.printf("%d년 %d월은 %d일 까지 있습니다. \n", inputYear, inputMonth, cal.maxDayOfMonth(inputYear, inputMonth));
+			System.out.println(prompt);
+			inputYear = sc.nextInt();
+			if(inputYear < 0) {
+				break;
+			}
+			while(true) {
+				System.out.println("달을 입력하세요.(1~12)");
+				System.out.println(prompt);
+				int inputMonth = sc.nextInt();
+				if(inputMonth == -1) {
+					break;
+				}
+				if(inputMonth > 12 || inputMonth < 0) {
+					System.out.println("1~12사이의 수를 입력하세요");
+					continue;
+				}
+				System.out.printf("%d년 %d월은 %d일 까지 있습니다. \n", inputYear, inputMonth, cal.maxDayOfMonth(inputYear, inputMonth));
+			}
 		}
 		System.out.println("종료합니다");
-		sc.close();
+		sc.close();*/
 		
 		
 	}
