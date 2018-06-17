7df7e92bc0711850ec0cd897478e1873d085b259
smallonestep <hankwan777@gmail.com>
2018-06-17 13:11:33 +0900

    Update usisng class method

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -3,6 +3,19 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	
+	private final static int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int maxDayOfMonth(int inputYear, int inputMonth) {
+		
+		if(inputYear % 4 == 0 && inputMonth == 2) {
+			return 29;
+		}else {
+			return MAX_DAYS[inputMonth-1];
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		// System.out.println("Hello, Calendar!");
 		// 개행문자 \n, \r
@@ -23,28 +36,25 @@ public class Calendar {
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		//배열에 넣어 강의 대로 만들어보기
-		int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		//int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		//최대일수는 정해진 상수이므로 static으로 빼서 사용해보기
+		//메소드 만들어 사용해보기
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("연도를 입력하세요");
 		System.out.println("달을 입력하세요.(1~12)");
 		int inputYear = sc.nextInt();
 		int inputMonth = sc.nextInt();
-		if(inputYear % 4 == 0 && inputMonth == 2) {
+		Calendar cal = new Calendar();
+		/*if(inputYear % 4 == 0 && inputMonth == 2) {
 			System.out.println(inputMonth + "월은 29일 까지 있습니다.");
 		}else {
 			//System.out.println(inputMonth + "월은 " + month[inputMonth-1] + "일 까지 있습니다.");
 			System.out.printf("%d월은 %d일 까지 있습니다. \n", inputMonth, month[inputMonth-1] );
-		}
+		}*/
+		System.out.printf("%d년 %d월은 %d일 까지 있습니다. \n", inputYear, inputMonth, cal.maxDayOfMonth(inputYear, inputMonth));
 		sc.close();
 		
-		//맵을 활용해서 출력하기
-		 
-		//내가 해보려는 것
-		// 8이상 8미만 으로 구분
-		//8미만 홀수는 31 , 2월은 예외, 윤달도 생각?
-		//8이상 홀수는 30
-		//enum을 써볼까?
-
 		
 	}
 }
