70653fed7b073ab9ed5df1bd74a53634d6ac877c
smallonestep <hankwan777@gmail.com>
2018-06-19 21:19:49 +0900

    윤년 조건 괄호 추가

--- src/honux/calendar/CalendarCalYM.java
+++ src/honux/calendar/CalendarCalYM.java
@@ -15,7 +15,7 @@ public class CalendarCalYM {
 		
 		boolean yoondal = false;
 		//if(year % 4 == 0 && year % 100 != 0 && year % 400 ==0) {
-		if(year % 4 == 0 && year % 100 != 0) {
+		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			yoondal = true;
 		}
 		
