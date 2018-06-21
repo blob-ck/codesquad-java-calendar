255d3483f559479ce7187f79287c8baf172b25b3
smallonestep <hankwan777@gmail.com>
2018-06-21 12:05:56 +0900

    일정등록 및 검색 완성

--- src/honux/calendar/CalendarCmd.java
+++ src/honux/calendar/CalendarCmd.java
@@ -1,25 +1,70 @@
 package honux.calendar;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class CalendarCmd {
 
 	private final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	private ArrayList<Integer> monthDays = new ArrayList<Integer>();
+	private HashMap<Date, String> planMap;
 	
-	//일정 조회
-	public void cmdSearch() {
-		System.out.println("[일정 등록]");
+	public CalendarCmd() {
+		planMap = new HashMap<Date, String>();
+	}
+	
+	//일정 검색
+	public void cmdSearch(Scanner scn) {
+		System.out.println("[일정 검색]");
+		System.out.print("날짜를 입력해 주세요. (yyyy-MM-dd)\n>");
+		String strDate = scn.next();
+		try {
+			Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+			System.out.println(date + "일정은 ");
+			System.out.println(planMap.get(date) + "입니다.");
+		} catch (ParseException e) {
+			System.out.println("yyyy-MM-dd 양식으로 날짜를 입력하세요");
+			cmdSearch(scn);
+		}
+		
+		
 	} // cmdSerch() end
 	
-	
 	//일정 등록
-	public void cmdRegister() {
-		System.out.println("[일정 검색]");
+	public void cmdRegister(Scanner scn) {
+		System.out.println("[일정 등록]");
+		System.out.print("날짜를 입력해 주세요. (yyyy-MM-dd)\n>");
+		String date = scn.next();
+		System.out.print("\n계획을 입력하세요.\n>");
+		String plan = "";
+		while(!plan.endsWith(";")) {
+			if(plan.equals("")) {
+				plan += scn.nextLine();
+			}else {
+				plan += "\n" + scn.nextLine();
+			}
+		}
+		//세미콜론 제거
+		plan = plan.substring(0, plan.length()-1);
+		try {
+			registerPlan(date, plan);
+		} catch (ParseException e) {
+			System.out.println("yyyy-MM-dd 양식으로 날짜를 입력하세요");
+			cmdRegister(scn);
+		}
 	} // cmdRegister() end
 	
+	public void registerPlan(String strDate, String plan) throws ParseException {
+			Date d = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+			planMap.put(d, plan);
+			System.out.println(d + "일정은 " + planMap.get(d));
+	}
+	
 	//도움말
-	public void cmdHelp() {
+	public void cmdHelp(Scanner scn) {
 		System.out.println("[도움말]");
 	} // cmdHelp() end
 	
@@ -29,6 +74,8 @@ public class CalendarCmd {
 		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			yoondal = true;
 		}
+		
+		//해당연도 직전월까지의 총 일 수
 		int nowYearDays = 0;
 		for (int i = 0; i < month-1; i++) {
 			nowYearDays += MAX_DAYS[i];
@@ -36,7 +83,9 @@ public class CalendarCmd {
 		if(yoondal && month > 2) {
 			nowYearDays ++;
 		}
+		//0연도부터 직전연도 + 해당연도 직전월까지 총 일 수
 		int totalDays = ((year - 1) * 365) + (year / 4) - (year / 100) + (year / 400) + nowYearDays;
+		//입력월의 시작일 요일을 계산하기 위함
 		int beginDays = totalDays % 7;
 		
 		for (int i = 0; i < beginDays + MAX_DAYS[month-1]; i++) {
@@ -71,25 +120,25 @@ public class CalendarCmd {
 	} // out() end
 
 	// 달력 출력
-	public void cmdCal(Scanner sc) {
+	public void cmdCal(Scanner scn) {
 		System.out.println("[달력 보기]");
 		if(!monthDays.isEmpty()) {
 			monthDays.clear();
 		}
 		System.out.println("연도를 입력하세요.");
 		System.out.print("YEAR > ");
-		int year = sc.nextInt();
+		int year = scn.nextInt();
 		if(year < 0) {
 			System.out.println("양수를 입력하세요");
-			cmdCal(sc);
+			cmdCal(scn);
 			return;
 		}
 		System.out.println("월을 입력하세요.");
 		System.out.print("MONTH > ");
-		int month = sc.nextInt();
+		int month = scn.nextInt();
 		if(month < 0 || month >12) {
 			System.out.println("1~12 사이의 숫자를 입력하세요");
-			cmdCal(sc);
+			cmdCal(scn);
 			return;
 		}
 		out(year, month);
--- src/honux/calendar/PromptMenu.java
+++ src/honux/calendar/PromptMenu.java
@@ -7,34 +7,38 @@ public class PromptMenu {
 	CalendarCmd cmd = new CalendarCmd();
 	
 	public void menu() {
-		System.out.println(  "+----------------------+\n"
-				+"| 1. 일정 등록\n"
-				+"| 2. 일정 검색\n"
-				+"| 3. 달력 보기\n"
-				+"| h. 도움말\n"
-				+"| q. 종료\n"
-				+"+----------------------+\n"
-				+"명령 (1, 2, 3, h, q)\n"
-				+"Command > ");
 		
 		Scanner scn = new Scanner(System.in);
-		while(true) {
+		boolean isLoop = true;
+		while(isLoop) {
+			System.out.println(  "+----------------------+\n"
+					+"| 1. 일정 등록\n"
+					+"| 2. 일정 검색\n"
+					+"| 3. 달력 보기\n"
+					+"| h. 도움말\n"
+					+"| q. 종료\n"
+					+"+----------------------+\n"
+					+"명령 (1, 2, 3, h, q)\n"
+					+"Command > ");
 			String command = scn.next();
-			if(command.equals("1")) {
-				cmd.cmdSearch();
-			}
-			else if(command.equals("2")) {
-				cmd.cmdRegister();
-			}		
-			else if(command.equals("3")) {
+			
+			switch (command) {
+			case "1":
+				cmd.cmdRegister(scn);
+				break;
+			case "2":
+				cmd.cmdSearch(scn);
+				break;
+			case "3":
 				cmd.cmdCal(scn);
-			}
-			else if(command.equals("h")) {
-				cmd.cmdHelp();
-			}	
-			else if(command.equals("q")) {
+				break;
+			case "h":
+				cmd.cmdHelp(scn);
+				break;
+			case "q":
 				System.out.println("[종료합니다.]");
 				scn.close();
+				isLoop = false;
 				break;
 			}
 		}
